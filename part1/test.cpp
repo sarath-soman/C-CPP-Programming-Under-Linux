1609648f6849573009e29933c233a5e6c455c4ec
Sarath Soman <sarath.s@keyvalue.systems>
2020-04-24 08:57:23 +0530

    Part1

---
+++ part1/test.cpp
@@ -0,0 +1,21 @@
+///////////////////////
+//
+//
+// A hello world program using GNU C/C++ under Linux
+//
+// g++ test.cpp => a.out
+// if you want to give another name (say test.exe ) for
+// the executable
+//
+// g++ -otest.exe test.cpp
+//
+// ./test.exe
+//
+//
+#include <stdio.h>
+
+int main( int argc , char **argv )
+{
+  printf("Hello world \n" );
+
+} 
