eaadc2c23db040498dc4c8b0aae7522b03c137f2
Sarath <nsarathsoman@gmail.com>
2020-04-24 21:28:35 +0530

    part11

---
+++ part11/exename.cpp
@@ -0,0 +1,16 @@
+// exename.cpp 
+
+#include <stdio.h>
+
+int main( int argc , char **argv )
+{
+
+  printf(argv[0]);  
+  printf("\n");
+  return 0;
+}
+
+// Eof exename.cpp
+//g++ -o exename.exe exename.cpp
+
+//./exename.exe 
