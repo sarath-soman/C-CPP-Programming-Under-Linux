3e470cc986cff183c0cdd9ad59f0e034bd0858fb
Sarath <nsarathsoman@gmail.com>
2020-04-25 19:09:02 +0530

    Part 13

---
+++ part13/main.cpp
@@ -0,0 +1,34 @@
+// test.cpp
+//
+//
+//g++ -o test.exe test.cpp
+//./test.exe
+//
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+int main()
+{
+  FILE *fp=0;
+
+  //--------- open a pipe to the ls command
+  //--------- second parameter (r) indicates read
+
+  if ( ( fp = popen("ls -l","r") ) == 0 )
+    return -1;
+
+  char bfr[8192];
+
+  //--------- Read line at a time and spit to the console
+
+  while ( !feof(fp) )
+  {
+       fgets(bfr,8192,fp);
+       printf(bfr);
+  }           
+
+  //-----------close the file
+  fclose(fp); 
+}
